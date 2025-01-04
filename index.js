515ef300a9ba98d8128b6af06ec57ba402ae5773
vansh432 <vanshdeep703@gmail.com>
2025-01-04 18:57:17 +0530

    changee done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import routes from './routes/index.js';
 import connectDB from './config/db.js'
+import cors from 'cors';
 const app = express();
 connectDB();
-
 app.use(express.json());
+app.use(cors());
 app.get('/',(req,res)=>{
     return res.status(200).json({
         status: "success",
